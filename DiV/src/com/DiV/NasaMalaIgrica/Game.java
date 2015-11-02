314f9018d2f44e7d7d1a81a224a318cae1774d6f
Thermain <spamtester223@gmail.com>
2015-11-02 15:10:04 +0100

    Pokusaj br. 1

--- DiV/src/com/DiV/NasaMalaIgrica/Game.java
+++ DiV/src/com/DiV/NasaMalaIgrica/Game.java
@@ -20,7 +20,7 @@ public class Game extends Canvas implements Runnable  {
 		thread.start();
 	}
 	public void run () {
-		System.out.println("Ako ovo uspes da skines, da promenis u uplodujes, uspeli smo");
+		System.out.println("Ako ovo uspes da skines, da promenis u uplodujes, uspeli SMO");
 		long lastTime = System.nanoTime();
 		double amountOfTicks = 60.0;
 		double ns = 1000000000 / amountOfTicks;
